4658ea131529cb609df825d04b789efc01ba559c
Frederick Beaulieu <frederick.beaulieu@canac.ca>
2026-04-08 11:25:41 -0400

    feat: --version with logo

--- internal/cli/args.go
+++ internal/cli/args.go
@@ -5,7 +5,39 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strings"
 )
+
+const logoArt = `
+⠴⠶⣖⡋⠉⠍⠑⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠊⠩⠉⢙⣲⠶⠦
+⠀⠀⠀⢣⠀⠀⠀⠀⠘⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠃⠀⠀⠀⠀⡜⠀⠀⠀
+⠀⠀⣠⠼⡆⠀⢀⡔⠊⠉⠉⠓⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠚⠉⠉⠑⢢⡀⠀⢰⠧⣄⠀⠀
+⠀⠀⠈⠚⡇⠀⠀⠃⠀⠀⠀⠀⠀⠀⠈⠓⢄⡀⠀⠀⠀⠀⠀⠀⠀ tuitui⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠚⠁⠀⠀⠀⠀⠀⠀⠘⠀⠀⢸⠓⠁⠀⠀
+⠀⠀⠀⠀⠘⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠢⣀⠀⠀⠀⠀⠀ %s⠀⠀⠀⠀⠀⠀⣀⠔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠲⣄⠀⠀⠀⠀⠀⠀⠒⠤⣀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⣀⠤⠒⠀⠀⠀⠀⠀⠀⣠⠖⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠤⢄⣀⣀⠀⠀⠀⢉⣓⠲⠀⣬⣕⡦⠀⠀⠀⠀  ⠀⠀⠀⠀⢴⣪⣥⠀⠖⣚⡉⠀⠀⠀⣀⣀⡠⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠿⠛⡶⠋⠁⠈⠑⣄⠘⢆⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⡰⠃⣠⠊⠁⠈⠙⢶⠛⠿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠎⢀⠞⠁⠀⠀⠀⠀⠈⢢⡈⠳⡀⠀⠀⠀  ⠀⠀⠀⢀⠞⢁⡔⠁⠀⠀⠀⠀⠈⠳⡀⠱⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠓⠒⠛⠂⠀⠀⠀⠀⠀⠀⠀⠱⡄⠙⣄⠀⠀  ⠀⠀⣠⠋⢠⠎⠀⠀⠀⠀⠀⠀⠀⠐⠛⠒⠚⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠦⢬⡦⠀  ⠀⢴⡥⠴⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`
+
+// centerVersion pads the version string so it appears centered
+// below "tuitui" in the logo. The slot is 6 characters wide,
+// matching the width of "tuitui" on the line above.
+func centerVersion(version string) string {
+	const slotWidth = 6
+	n := len(version)
+	if n >= slotWidth {
+		return version[:slotWidth]
+	}
+	pad := (slotWidth - n) / 2
+	right := slotWidth - pad - n
+	return strings.Repeat(" ", pad) + version + strings.Repeat(" ", right)
+}
+
+func logo(version string) string {
+	return fmt.Sprintf(logoArt, centerVersion(version))
+}
 
 type Args struct {
 	RepoPath string
@@ -14,6 +46,7 @@ type Args struct {
 func Parse(version string) Args {
 	args := os.Args[1:]
 
+	handleHelp(args, version)
 	handleVersion(args, version)
 
 	var parsed Args
@@ -32,7 +65,25 @@ func Parse(version string) Args {
 
 func handleVersion(args []string, version string) {
 	if slices.Contains(args, "--version") || slices.Contains(args, "-v") {
-		fmt.Println("tuitui " + version)
+		fmt.Println(logo(version))
+		os.Exit(0)
+	}
+}
+
+func handleHelp(args []string, version string) {
+	if slices.Contains(args, "--help") || slices.Contains(args, "-h") {
+		fmt.Println(logo(version))
+		fmt.Println()
+		fmt.Println("A terminal user interface for Jujutsu (jj)")
+		fmt.Println()
+		fmt.Println("Usage: tuitui [path]")
+		fmt.Println()
+		fmt.Println("Arguments:")
+		fmt.Println("  [path]    Path to a jj repository (defaults to current directory)")
+		fmt.Println()
+		fmt.Println("Flags:")
+		fmt.Println("  -h, --help       Show this help message")
+		fmt.Println("  -v, --version    Show version")
 		os.Exit(0)
 	}
 }
